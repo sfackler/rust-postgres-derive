b9776a3b0a1244f6fba6dbdb22838e8cd7d52577
Steven Fackler <sfackler@gmail.com>
2016-02-19 20:04:25 -0800

    Add more tests

--- postgres-derive-macros/tests/domains.rs
+++ postgres-derive-macros/tests/domains.rs
@@ -5,6 +5,8 @@
 extern crate postgres;
 
 use postgres::{Connection, SslMode};
+use postgres::error::Error;
+use postgres::types::WrongType;
 
 mod util;
 
@@ -34,4 +36,35 @@ fn name_overrides() {
 
     util::test_type(&conn, "session_id", &[(SessionId(b"0123456789abcdef".to_vec()),
                                             "'0123456789abcdef'")]);
+}
+
+#[test]
+fn wrong_name() {
+    #[derive(FromSql, ToSql, Debug, PartialEq)]
+    struct SessionId(Vec<u8>);
+
+    let conn = Connection::connect("postgres://postgres@localhost", SslMode::None).unwrap();
+    conn.execute("CREATE DOMAIN pg_temp.session_id AS bytea CHECK(octet_length(VALUE) = 16);", &[])
+        .unwrap();
+
+    match conn.execute("SELECT $1::session_id", &[&SessionId(vec![])]) {
+        Err(Error::Conversion(ref r)) if r.is::<WrongType>() => {}
+        v => panic!("unexpected response {:?}", v),
+    }
+}
+
+#[test]
+fn wrong_type() {
+    #[derive(FromSql, ToSql, Debug, PartialEq)]
+    #[postgres(name = "session_id")]
+    struct SessionId(i32);
+
+    let conn = Connection::connect("postgres://postgres@localhost", SslMode::None).unwrap();
+    conn.execute("CREATE DOMAIN pg_temp.session_id AS bytea CHECK(octet_length(VALUE) = 16);", &[])
+        .unwrap();
+
+    match conn.execute("SELECT $1::session_id", &[&SessionId(0)]) {
+        Err(Error::Conversion(ref r)) if r.is::<WrongType>() => {}
+        v => panic!("unexpected response {:?}", v),
+    }
 }
--- postgres-derive-macros/tests/enums.rs
+++ postgres-derive-macros/tests/enums.rs
@@ -5,6 +5,8 @@
 extern crate postgres;
 
 use postgres::{Connection, SslMode};
+use postgres::error::Error;
+use postgres::types::WrongType;
 
 mod util;
 
@@ -41,4 +43,57 @@ fn name_overrides() {
     util::test_type(&conn,
                     "mood",
                     &[(Mood::Sad, "'sad'"), (Mood::Ok, "'ok'"), (Mood::Happy, "'happy'")]);
+}
+
+#[test]
+fn wrong_name() {
+    #[derive(Debug, ToSql, FromSql, PartialEq)]
+    enum Foo {
+        Bar,
+        Baz
+    }
+
+    let conn = Connection::connect("postgres://postgres@localhost", SslMode::None).unwrap();
+    conn.execute("CREATE TYPE pg_temp.foo AS ENUM ('Bar', 'Baz')", &[]).unwrap();
+
+    match conn.execute("SELECT $1::foo", &[&Foo::Bar]) {
+        Err(Error::Conversion(ref r)) if r.is::<WrongType>() => {}
+        v => panic!("unexpected response {:?}", v),
+    }
+}
+
+#[test]
+fn extra_variant() {
+    #[derive(Debug, ToSql, FromSql, PartialEq)]
+    #[postgres(name = "foo")]
+    enum Foo {
+        Bar,
+        Baz,
+        Buz,
+    }
+
+    let conn = Connection::connect("postgres://postgres@localhost", SslMode::None).unwrap();
+    conn.execute("CREATE TYPE pg_temp.foo AS ENUM ('Bar', 'Baz')", &[]).unwrap();
+
+    match conn.execute("SELECT $1::foo", &[&Foo::Bar]) {
+        Err(Error::Conversion(ref r)) if r.is::<WrongType>() => {}
+        v => panic!("unexpected response {:?}", v),
+    }
+}
+
+#[test]
+fn missing_variant() {
+    #[derive(Debug, ToSql, FromSql, PartialEq)]
+    #[postgres(name = "foo")]
+    enum Foo {
+        Bar,
+    }
+
+    let conn = Connection::connect("postgres://postgres@localhost", SslMode::None).unwrap();
+    conn.execute("CREATE TYPE pg_temp.foo AS ENUM ('Bar', 'Baz')", &[]).unwrap();
+
+    match conn.execute("SELECT $1::foo", &[&Foo::Bar]) {
+        Err(Error::Conversion(ref r)) if r.is::<WrongType>() => {}
+        v => panic!("unexpected response {:?}", v),
+    }
 }
