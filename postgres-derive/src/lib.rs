27b02125247523b4767086c8f4ca4c5df1fdb1c8
Steven Fackler <sfackler@palantir.com>
2016-10-04 22:22:49 -0400

    Rename codegen2 to internals

--- postgres-derive/src/lib.rs
+++ postgres-derive/src/lib.rs
@@ -1,13 +1,13 @@
 #![feature(rustc_macro, rustc_macro_lib)]
 
 extern crate rustc_macro;
-extern crate postgres_derive_codegen2;
+extern crate postgres_derive_internals;
 
 use rustc_macro::TokenStream;
 
 #[rustc_macro_derive(ToSql)]
 pub fn derive_tosql(input: TokenStream) -> TokenStream {
-    match postgres_derive_codegen2::expand_derive_tosql(&input.to_string()) {
+    match postgres_derive_internals::expand_derive_tosql(&input.to_string()) {
         Ok(expanded) => expanded.parse().unwrap(),
         Err(err) => panic!(err),
     }
