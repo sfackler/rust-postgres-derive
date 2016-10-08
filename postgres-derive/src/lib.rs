6e185c06a39b5fe6a7cc669f191ee2d96acd4078
Steven Fackler <sfackler@gmail.com>
2016-10-08 10:55:49 -0700

    Update for new nightly

--- postgres-derive/src/lib.rs
+++ postgres-derive/src/lib.rs
@@ -1,16 +1,16 @@
-#![feature(rustc_macro, rustc_macro_lib)]
+#![feature(proc_macro, proc_macro_lib)]
 
-extern crate rustc_macro;
+extern crate proc_macro;
 extern crate postgres_derive_internals;
 
-use rustc_macro::TokenStream;
+use proc_macro::TokenStream;
 
-#[rustc_macro_derive(ToSql)]
+#[proc_macro_derive(ToSql)]
 pub fn derive_tosql(input: TokenStream) -> TokenStream {
     derive("ToSql", input)
 }
 
-#[rustc_macro_derive(FromSql)]
+#[proc_macro_derive(FromSql)]
 pub fn derive_fromsql(input: TokenStream) -> TokenStream {
     derive("FromSql", input)
 }
