90580d2e0130660fe3ce220a620f8693eaf26c9a
Steven Fackler <sfackler@gmail.com>
2016-10-07 23:05:32 -0700

    Remove postgres-derive-macros

--- postgres-derive-macros/src/lib.rs
+++
@@ -1,9 +0,0 @@
-#![feature(plugin_registrar, rustc_private)]
-
-extern crate rustc_plugin;
-extern crate postgres_derive_codegen;
-
-#[plugin_registrar]
-pub fn plugin_registrar(reg: &mut rustc_plugin::Registry) {
-    postgres_derive_codegen::register(reg);
-}
