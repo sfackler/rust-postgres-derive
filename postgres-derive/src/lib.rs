2b4c77b1f87712184bc6b0259c01f4be1ac58563
Steven Fackler <sfackler@palantir.com>
2016-11-15 21:53:26 +0100

    Update to new attribute whitelisting setup

--- postgres-derive/src/lib.rs
+++ postgres-derive/src/lib.rs
@@ -2,45 +2,24 @@
 
 extern crate proc_macro;
 extern crate postgres_derive_internals;
-#[macro_use]
-extern crate post_expansion;
+
 extern crate syn;
-extern crate quote;
 
 use proc_macro::TokenStream;
-use quote::{ToTokens, Tokens};
 
-register_post_expansion!(PostExpansion_postgres_derive);
-
-#[proc_macro_derive(ToSql)]
+#[proc_macro_derive(ToSql, attributes(postgres))]
 pub fn derive_tosql(input: TokenStream) -> TokenStream {
     derive(input, postgres_derive_internals::expand_derive_tosql)
 }
 
-#[proc_macro_derive(FromSql)]
+#[proc_macro_derive(FromSql, attributes(postgres))]
 pub fn derive_fromsql(input: TokenStream) -> TokenStream {
     derive(input, postgres_derive_internals::expand_derive_fromsql)
 }
 
 fn derive(input: TokenStream, expand: fn(&str) -> Result<String, String>) -> TokenStream {
-    let source = input.to_string();
-
-    let decl = expand_decl(&source);
-
-    let impl_ = match expand(&source) {
-        Ok(impl_) => impl_,
+    match expand(&input.to_string()) {
+        Ok(impl_) => impl_.parse().unwrap(),
         Err(e) => panic!("{}", e),
-    };
-
-    let expanded = format!("{}\n{}", decl, impl_);
-    expanded.parse().unwrap()
-}
-
-fn expand_decl(source: &str) -> String {
-    let ast = syn::parse_macro_input(source).unwrap();
-    let stripped = post_expansion::strip_attrs_later(ast, &["postgres"], "postgres_derive");
-
-    let mut tokens = Tokens::new();
-    stripped.to_tokens(&mut tokens);
-    tokens.to_string()
+    }
 }
